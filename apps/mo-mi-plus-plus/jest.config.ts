1eac03da1f6087adf0bbcd56e707547eed8acf67
Zachary Morgan <zsmorgan21@gmail.com>
2023-02-11 22:56:37 -0500

    Initial commit

---
+++ apps/mo-mi-plus-plus/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'mo-mi-plus-plus',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/mo-mi-plus-plus',
+};
